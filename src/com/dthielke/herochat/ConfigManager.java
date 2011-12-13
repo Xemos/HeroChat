a5d14e78202fc37c416caff67880b3f6ef1d9ff9
DThielke <dave.thielke@gmail.com>
2011-12-13 02:24:10 -0500

    Save default config settings if not found.

--- src/com/dthielke/herochat/ConfigManager.java
+++ src/com/dthielke/herochat/ConfigManager.java
@@ -1,15 +1,28 @@
 package com.dthielke.herochat;
 
 import org.bukkit.ChatColor;
-import org.bukkit.util.config.Configuration;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.MemoryConfiguration;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class ConfigManager {
 
     public void load(File file) {
-        Configuration config = new Configuration(file);
-        config.load();
+        FileConfiguration config = new YamlConfiguration();
+        try {
+            if (file.exists())
+                config.load(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+        config.setDefaults(getDefaults());
 
         ChannelManager channelManager = HeroChat.getChannelManager();
 
@@ -34,6 +47,8 @@ public class ConfigManager {
             channelManager.addModPermission(Chatter.Permission.MODIFY_FORMAT);
         if (config.getBoolean("moderator-permissions.can-modify-shortcut", false))
             channelManager.addModPermission(Chatter.Permission.MODIFY_SHORTCUT);
+        if (config.getBoolean("moderator-permissions.can-color-messages", true))
+            channelManager.addModPermission(Chatter.Permission.COLOR);
 
         // make sure we have at least one channel (create one if we don't)
         if (channelManager.getChannels().isEmpty()) {
@@ -48,9 +63,32 @@ public class ConfigManager {
             channelManager.setDefaultChannel(channelManager.getDefaultChannel());
 
         // load censors
-        HeroChat.getMessageHandler().setCensors(config.getStringList("censors", null));
+        HeroChat.getMessageHandler().setCensors(config.getStringList("censors"));
 
-        config.save();
+        try {
+            config.options().copyDefaults(true);
+            config.save(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private MemoryConfiguration getDefaults() {
+        MemoryConfiguration config = new MemoryConfiguration();
+        config.set("moderator-permissions.can-kick", true);
+        config.set("moderator-permissions.can-ban", true);
+        config.set("moderator-permissions.can-mute", true);
+        config.set("moderator-permissions.can-remove-channel", true);
+        config.set("moderator-permissions.can-modify-nick", true);
+        config.set("moderator-permissions.can-modify-color", true);
+        config.set("moderator-permissions.can-modify-distance", true);
+        config.set("moderator-permissions.can-modify-password", true);
+        config.set("moderator-permissions.can-modify-format", false);
+        config.set("moderator-permissions.can-modify-shortcut", false);
+        config.set("moderator-permissions.can-color-messages", true);
+        config.set("default-channel", "Global");
+        config.set("censors", new ArrayList<String>());
+        return config;
     }
 
 }
